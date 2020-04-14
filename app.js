4eeb54bdb365bcea8c4e8befb9fd24a60b5c5e01
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-04-15 04:05:02 +0530

    Connect to cluster and add dot env file

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//     useNewUrlParser: true
+// });
+// client.connect(err => {
+//     const collection = client.db(process.env.DB_NAME).collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(res => console.log("Connected to DB"))
+    .catch(err => console.log(err))
 
 app.use(
     '/graphql',
